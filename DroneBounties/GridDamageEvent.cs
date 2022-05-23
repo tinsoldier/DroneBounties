d15b81014c4d3f59dccd08737b11222b53348b23
David Jones <david.jones@swanisland.net>
2022-05-23 16:17:50 -0700

    Fix: preprocessor glitch move types to new file

--- DroneBounties/GridDamageEvent.cs
+++ DroneBounties/GridDamageEvent.cs
@@ -1,16 +1,11 @@
 ﻿namespace DroneBounties
 {
-    partial class KillRewardSession
+    internal class GridDamageEvent
     {
-        #endregion
-
-        internal class GridDamageEvent
-        {
-            //Not guaranteed to be "enemies", that is verified on kill
-            public long AttackerIdentityId { get; set; }
-            public long VictimEntityId { get; set; } 
-            public float Damage { get; set; }
-            public long TimestampTicks { get; set; }
-        }
+        //Not guaranteed to be "enemies", that is verified on kill
+        public long AttackerIdentityId { get; set; }
+        public long VictimEntityId { get; set; } 
+        public float Damage { get; set; }
+        public long TimestampTicks { get; set; }
     }
 }
